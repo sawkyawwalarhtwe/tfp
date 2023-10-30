4ce9afe8ca01e6c81a95224a24b2fe22b80e3b31
Saw Kyaw Walar Htwe <107252451+sawkyawwalarhtwe@users.noreply.github.com>
2023-10-31 00:09:22 +0630

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,32 @@
+
+import NavBar from '../components/navigation'
+import Footer from '../components/footer'
+
+import SlideShow from './slideshow'
+import MAbout from './about'
+import Join from './joinSection'
+import MPublication from './publication'
+import MainNews from './news'
+import MupcomingTraining from './training'
+import MEvents from './events'
+
+export default function HomePage() {
+  return (
+    <main className=' w-full overflow-hidden'>
+      <NavBar/>
+      <SlideShow/>
+      <hr className='mx-[6%]'/>   
+      <MAbout/>
+      <hr className='mx-[6%]'/>
+      <MainNews/>
+      <MEvents/>
+      <hr className='mx-[6%]'/>
+      <Join/>
+      <hr className='mx-[6%]'/>
+      <MupcomingTraining></MupcomingTraining>
+      <hr className='mx-[6%]'/>
+      <MPublication/>
+      <Footer/>
+    </main>
+  )
+}
